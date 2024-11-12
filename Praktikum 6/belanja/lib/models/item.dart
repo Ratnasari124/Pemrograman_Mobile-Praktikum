5398869c06ea195159ba328b0e475ceb918f42ff
Ratnasari <ratnasari121729@gmail.com>
2024-11-12 12:04:44 +0700

    Praktikum 5 dan Tugas 2

---
+++ Praktikum 6/belanja/lib/models/item.dart
@@ -0,0 +1,16 @@
+class Item {
+  String name;
+  int price;
+  String photo;
+  int stock;
+  double rating;
+
+  Item({
+    required this.name,
+    required this.price,
+    required this.photo,
+    required this.stock,
+    required this.rating,
+  });
+
+}
